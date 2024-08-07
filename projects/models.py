5db1bff0c221ccecba5eaa8a633dc1f0c7cb95ca
dipan-22 <dipandutta944@gmail.com>
2024-08-07 12:54:01 +0530

    signals added

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 import uuid
+from users.models import Profile
 # Create your models here.
 
 
 class Project(models.Model):
+    owner=models.ForeignKey(Profile,null=True,blank=True,on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True,default="default.jpg")
@@ -13,8 +15,7 @@ class Project(models.Model):
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=True)
 
     def __str__(self):
         return self.title
